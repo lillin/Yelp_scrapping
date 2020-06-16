a87326d5a0b8db9f8f5d24afd622b61bdf239287
a.rudkovska <a.rudkovska@riseapps.biz>
2020-06-16 16:38:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+LINKS_LIST = [
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Arlington%2C%20VA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Seattle%2C%20WA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Minneapolis%2C%20MN',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=San%20Francisco%2C%20CA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Madison%2C%20WI',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Washington%2C%20DC',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Saint%20Paul%2C%20MN',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Irvine%2C%20CA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Denver%2C%20CO',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Portland%2C%20OR',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Miami%2C%20FL',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=New%20York%2C%20NY',
+]
+
+
+class Parser:
+    COMPANY_NAME = 'company_name'
+    CATEGORY = 'category'
+    WEBSITE = 'website'
+
+    LINKS_CATALOG = []
+    FIELDNAMES = [COMPANY_NAME, CATEGORY, WEBSITE, ]
+
+    def __init__(self):
+
+
+
+        pass
