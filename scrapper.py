a5971e25508773b779a87f3089a095a44dfdfd45
a.rudkovska <a.rudkovska@riseapps.biz>
2020-06-18 19:00:38 +0300

    Implement getting of info from pages, add write links to file

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-# import csv
+import csv
 
 import os
 
@@ -8,28 +8,28 @@ from selenium.webdriver.firefox.options import Options
 
 LINKS_LIST = [
     'https://www.yelp.com/search?cflt=gyms&find_loc=Arlington%2C%20VA',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Seattle%2C%20WA',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Minneapolis%2C%20MN',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=San%20Francisco%2C%20CA',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Madison%2C%20WI',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Washington%2C%20DC',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Saint%20Paul%2C%20MN',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Irvine%2C%20CA',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Denver%2C%20CO',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Portland%2C%20OR',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=Miami%2C%20FL',
-    'https://www.yelp.com/search?cflt=gyms&find_loc=New%20York%2C%20NY',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Seattle%2C%20WA',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Minneapolis%2C%20MN',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=San%20Francisco%2C%20CA',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Madison%2C%20WI',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Washington%2C%20DC',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Saint%20Paul%2C%20MN',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Irvine%2C%20CA',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Denver%2C%20CO',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Portland%2C%20OR',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Miami%2C%20FL',
+    # 'https://www.yelp.com/search?cflt=gyms&find_loc=New%20York%2C%20NY',
 ]
 
 
 class Scrapper:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    LINKS_CATALOG_FILE = 'links_catalog.csv'
 
     COMPANY_NAME = 'company_name'
     CATEGORY = 'category'
     WEBSITE = 'website'
 
-    LINKS_CATALOG = []
     FIELDNAMES = [COMPANY_NAME, CATEGORY, WEBSITE, ]
 
     def __init__(self):
@@ -40,23 +40,30 @@ class Scrapper:
         self.browser = webdriver.Firefox(executable_path=self.driver, options=self.options)
         self.browser.implicitly_wait(10)
 
-        # TODO: uncomment after testing
-        # self.file = open('companies.csv', 'w')
-        # self.writer = \
-        #     csv.DictWriter(self.file, fieldnames=self.FIELDNAMES)
-        #
-        # self.writer.writeheader()
+        self.links_catalog = open(self.LINKS_CATALOG_FILE, 'w')
+        self.catalog_writer = csv.writer(self.links_catalog)
+
+        self.file = open('companies.csv', 'w')
+        self.writer = \
+            csv.DictWriter(self.file, fieldnames=self.FIELDNAMES)
+
+        self.writer.writeheader()
 
     def __call__(self, *args, **kwargs):
         self.get_info()
-        # self.scrap_info()
+        self.scrap_info()
 
         self.browser.close()
+        self.file.close()
 
     def get_info(self):
         for link in LINKS_LIST:
-            self.browser.get(link)  # go to page from companies list
+            self.browser.get(link)
 
+            print('-'*20)
+            print('NEW AREA: ')
+            print(link)
+            print('-'*20)
             while True:
                 try:
                     self.get_companies_list_from_page()
@@ -69,6 +76,7 @@ class Scrapper:
 
                 except NoSuchElementException:
                     break
+        self.links_catalog.close()
 
     def get_companies_list_from_page(self):
         page_h4_headers = self.browser.find_elements_by_tag_name('h4')
@@ -76,20 +84,45 @@ class Scrapper:
         for elem in page_h4_headers:
             anchor_elem = elem.find_element_by_tag_name('a')
             link_to_company = anchor_elem.get_property('href')
-            self.LINKS_CATALOG.append(link_to_company)
+            print(link_to_company)
+            self.catalog_writer.writerow(link_to_company)
 
     def scrap_info(self):
-        for link in self.LINKS_CATALOG:
-            data = dict.fromkeys(self.FIELDNAMES)
+        with open(self.LINKS_CATALOG_FILE, 'r') as links_catalog:
+            for link in links_catalog:
+                data = dict.fromkeys(self.FIELDNAMES)
 
-            self.browser.get(link)
+                self.browser.get(link)
 
-            # TODO: implement find_elem
-            data[self.COMPANY_NAME] = None
-            data[self.CATEGORY] = None
-            data[self.WEBSITE] = None
+                data[self.COMPANY_NAME] = self.browser.find_element_by_tag_name('h1').text
 
-            # self.writer.writerow(data)
+                categories_containers = \
+                    self.browser.find_element_by_xpath(
+                        '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[1]/div[1]/div/div/span'
+                    ).find_elements_by_tag_name('span')
+
+                try:
+                    categories = list()
+                    for elem in categories_containers:
+                        categories.append(elem.find_element_by_tag_name('a').text)
+                    data[self.CATEGORY] = ', '.join(categories)
+                except NoSuchElementException:
+                    print('EXCEPTION: ', link)
+                    pass
+
+                try:
+                    data[self.WEBSITE] = \
+                        self.browser.find_element_by_xpath(
+                            '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[2]/div/div/section/div/div[1]/div/div[2]/p[2]/a'
+                        ).text
+                except NoSuchElementException:
+                    pass
+
+                print()
+                print('OUTPUT: ')
+                print(data)
+
+                self.writer.writerow(data)
 
 
 if __name__ == '__main__':
