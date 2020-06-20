2737fe32fd2aca7688f3a7cf461ef5f71e1e1ba6
a.rudkovska <a.rudkovska@riseapps.biz>
2020-06-20 10:04:25 +0300

    Finish single page parsing, improve design

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,4 @@
 import csv
-
 import os
 
 from selenium import webdriver
@@ -8,23 +7,24 @@ from selenium.webdriver.firefox.options import Options
 
 LINKS_LIST = [
     'https://www.yelp.com/search?cflt=gyms&find_loc=Arlington%2C%20VA',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Seattle%2C%20WA',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Minneapolis%2C%20MN',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=San%20Francisco%2C%20CA',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Madison%2C%20WI',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Washington%2C%20DC',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Saint%20Paul%2C%20MN',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Irvine%2C%20CA',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Denver%2C%20CO',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Portland%2C%20OR',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=Miami%2C%20FL',
-    # 'https://www.yelp.com/search?cflt=gyms&find_loc=New%20York%2C%20NY',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Seattle%2C%20WA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Minneapolis%2C%20MN',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=San%20Francisco%2C%20CA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Madison%2C%20WI',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Washington%2C%20DC',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Saint%20Paul%2C%20MN',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Irvine%2C%20CA',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Denver%2C%20CO',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Portland%2C%20OR',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=Miami%2C%20FL',
+    'https://www.yelp.com/search?cflt=gyms&find_loc=New%20York%2C%20NY',
 ]
 
 
 class Scrapper:
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    LINKS_CATALOG_FILE = 'links_catalog.csv'
+    BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Yelp_scrapping')
+    LINKS_CATALOG_FILE = os.path.join(BASE_DIR, 'resulting_files', 'links_catalog.csv')
+    COMPANIES_FILE = os.path.join(BASE_DIR, 'resulting_files', 'companies.csv')
 
     COMPANY_NAME = 'company_name'
     CATEGORY = 'category'
@@ -32,98 +32,155 @@ class Scrapper:
 
     FIELDNAMES = [COMPANY_NAME, CATEGORY, WEBSITE, ]
 
+    RESULTS_TEMPLATE = dict.fromkeys(FIELDNAMES)
+
     def __init__(self):
-        self.driver = os.path.join(self.BASE_DIR, 'Yelp_scrapping', 'geckodriver.exe')
+        self.driver = os.path.join(self.BASE_DIR, 'geckodriver', 'geckodriver.exe')
         self.options = Options()
         self.options.add_argument('-headless')
 
         self.browser = webdriver.Firefox(executable_path=self.driver, options=self.options)
         self.browser.implicitly_wait(10)
 
-        self.links_catalog = open(self.LINKS_CATALOG_FILE, 'w')
-        self.catalog_writer = csv.writer(self.links_catalog)
-
-        self.file = open('companies.csv', 'w')
+        self.file = open(self.COMPANIES_FILE, 'w')
         self.writer = \
             csv.DictWriter(self.file, fieldnames=self.FIELDNAMES)
 
         self.writer.writeheader()
 
     def __call__(self, *args, **kwargs):
-        self.get_info()
         self.scrap_info()
 
         self.browser.close()
         self.file.close()
 
+    def init_writers(self):
+        links_catalog = open(self.LINKS_CATALOG_FILE, 'w')
+        catalog_writer = csv.writer(links_catalog)
+
+        setattr(self, 'links_catalog', links_catalog)
+        setattr(self, 'catalog_writer', catalog_writer)
+
     def get_info(self):
+        self.init_writers()
+
         for link in LINKS_LIST:
             self.browser.get(link)
 
-            print('-'*20)
-            print('NEW AREA: ')
-            print(link)
-            print('-'*20)
             while True:
                 try:
-                    self.get_companies_list_from_page()
+                    self._get_companies_list_from_page()
 
                     next_page_link = self.browser.find_element_by_xpath(
                         '/html/body/div[2]/div[3]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div/div[11]/span'
                     ).find_element_by_tag_name('a').get_property('href')
-
                     self.browser.get(next_page_link)
-
                 except NoSuchElementException:
                     break
-        self.links_catalog.close()
 
-    def get_companies_list_from_page(self):
+        getattr(self, 'links_catalog').close()
+
+    def scrap_info(self):
+        if os.path.getsize(self.LINKS_CATALOG_FILE) > 0:
+            with open(self.LINKS_CATALOG_FILE, 'r') as links_catalog:
+                for link in links_catalog:
+
+                    self.browser.get(link)
+                    print('scrap_info'.upper(), link)
+                    results = self._compose_resulting_info()
+
+                    print('OUTPUT: ')
+                    print(results)
+
+                    self.writer.writerow(results)
+        else:
+            self.get_info()
+            self.scrap_info()
+
+    def _get_companies_list_from_page(self):
         page_h4_headers = self.browser.find_elements_by_tag_name('h4')
 
         for elem in page_h4_headers:
             anchor_elem = elem.find_element_by_tag_name('a')
             link_to_company = anchor_elem.get_property('href')
+
             print(link_to_company)
-            self.catalog_writer.writerow(link_to_company)
+            getattr(self, 'catalog_writer').writerow([link_to_company])
 
-    def scrap_info(self):
-        with open(self.LINKS_CATALOG_FILE, 'r') as links_catalog:
-            for link in links_catalog:
-                data = dict.fromkeys(self.FIELDNAMES)
+    def _compose_resulting_info(self):
+        data = self.RESULTS_TEMPLATE.copy()
 
-                self.browser.get(link)
+        data[self.COMPANY_NAME] = self._get_company_name()
+        data[self.CATEGORY] = self._get_categories()
+        data[self.WEBSITE] = self._get_website()
 
-                data[self.COMPANY_NAME] = self.browser.find_element_by_tag_name('h1').text
+        return data
 
-                categories_containers = \
-                    self.browser.find_element_by_xpath(
-                        '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[1]/div[1]/div/div/span'
+    def _get_company_name(self):
+        try:
+            return self.browser.find_element_by_tag_name('h1').text
+        except NoSuchElementException:
+            pass
+
+    def _get_categories(self):
+        pattern_1 = \
+            '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[1]/div[1]/div/div/span'
+
+        pattern_2 = \
+            '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[1]/div[1]/div/div/span[2]'
+
+        pattern_3 = \
+            '/html/body/div[2]/div[4]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div/span'
+
+        categories = list()
+        categories_containers = None
+
+        try:
+            categories_containers = \
+                self.browser.find_element_by_xpath(
+                    pattern_1
+                ).find_elements_by_tag_name('span')
+        except NoSuchElementException:
+            try:
+                categories_containers = self.browser.find_element_by_xpath(
+                        pattern_2
                     ).find_elements_by_tag_name('span')
-
+            except NoSuchElementException:
                 try:
-                    categories = list()
-                    for elem in categories_containers:
-                        categories.append(elem.find_element_by_tag_name('a').text)
-                    data[self.CATEGORY] = ', '.join(categories)
-                except NoSuchElementException:
-                    print('EXCEPTION: ', link)
-                    pass
-
-                try:
-                    data[self.WEBSITE] = \
-                        self.browser.find_element_by_xpath(
-                            '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[2]/div/div/section/div/div[1]/div/div[2]/p[2]/a'
-                        ).text
+                    categories_containers = self.browser.find_element_by_xpath(
+                        pattern_3
+                    ).find_elements_by_tag_name('span')
                 except NoSuchElementException:
                     pass
 
-                print()
-                print('OUTPUT: ')
-                print(data)
+        if categories_containers:
+            try:
+                for elem in categories_containers:
+                    categories.append(elem.find_element_by_tag_name('a').text)
+            except NoSuchElementException:
+                pass
 
-                self.writer.writerow(data)
+        return ', '.join(categories)
+
+    def _get_website(self):
+        pattern = \
+            '/html/body/div[2]/div[4]/div/div[4]/div/div/div[2]/div[2]/div/div/section/div/div[1]/div/div[2]/p[2]/a'
+
+        try:
+            return \
+                self.browser.find_element_by_xpath(
+                    pattern
+                ).text
+        except NoSuchElementException:
+            pass
 
 
 if __name__ == '__main__':
-    Scrapper()()
+    scrapper = Scrapper()
+
+    try:
+        scrapper()
+    except Exception as e:
+        print(e)
+        scrapper.browser.close()
+        scrapper.file.close()
